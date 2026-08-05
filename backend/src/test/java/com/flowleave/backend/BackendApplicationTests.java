a2fe8bdf97a98921209fd95611d0db1a05327660
bhagathkrishnasunilkumar.23cse@kongu.edu <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-08-05 15:07:53 +0530

    Chore : Initialized spring boot backend project

---
+++ backend/src/test/java/com/flowleave/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.flowleave.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
