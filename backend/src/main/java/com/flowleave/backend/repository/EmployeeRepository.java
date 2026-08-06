d3457c5e782ce679b86c77625a00b42501913b4e
bhagathkrishnasunilkumar.23cse@kongu.edu <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-08-06 15:48:43 +0530

    refactor(employee): improve repository methods

--- backend/src/main/java/com/flowleave/backend/repository/EmployeeRepository.java
+++ backend/src/main/java/com/flowleave/backend/repository/EmployeeRepository.java
@@ -9,5 +9,9 @@ public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
     Optional<Employee> findByEmail(String email);
 
-    Optional<Employee> findByEmailAndPassword(String email, String password);
+    Optional<Employee> findByEmployeeCode(String employeeCode);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByEmployeeCode(String employeeCode);
 }
