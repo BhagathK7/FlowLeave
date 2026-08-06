6b2c3cb6f63b53feb1c13adb0d92702c0cc58e7a
bhagathkrishnasunilkumar.23cse@kongu.edu <bhagathkrishnasunilkumar.23cse@kongu.edu>
2026-08-06 15:12:30 +0530

    feat(frontend): setup global theme and login placeholder

---
+++ frontend/src/pages/auth/Login.jsx
@@ -0,0 +1,24 @@
+function Login() {
+
+    return (
+
+        <div
+            style={{
+                height:"100vh",
+                display:"flex",
+                alignItems:"center",
+                justifyContent:"center",
+                fontSize:"2rem",
+                fontWeight:"700"
+            }}
+        >
+
+            FlowLeave Login
+
+        </div>
+
+    );
+
+}
+
+export default Login;
